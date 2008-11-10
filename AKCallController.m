dbcc9d79781a8b6a1ff3fd03e4c30a5cbe446cc1
eofster <devnull@localhost>
2008-11-10 11:56:20 +0000

    Added license info to NSString categories. Corrected capitalization of "rights reserved" in all other files.

--- AKCallController.m
+++ AKCallController.m
@@ -2,7 +2,7 @@
 //  AKCallController.m
 //  Telephone
 //
-//  Copyright (c) 2008 Alexei Kuznetsov. All Rights Reserved.
+//  Copyright (c) 2008 Alexei Kuznetsov. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are met:
--- AKTelephoneAccount.m
+++ AKTelephoneAccount.m
@@ -2,7 +2,7 @@
 //  AKTelephoneAccount.m
 //  Telephone
 //
-//  Copyright (c) 2008 Alexei Kuznetsov. All Rights Reserved.
+//  Copyright (c) 2008 Alexei Kuznetsov. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without
 //  modification, are permitted provided that the following conditions are met:
