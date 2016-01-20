eecebc953347ba9bda917671a3cbcfb2b6549384
Alexei Kuznetsov <mail@eofster.com>
2016-01-20 20:11:11 +0100

    Add missing copyright notice
    Issue #167

--- Telephone/AKNSString+Escaping.m
+++ Telephone/AKNSString+Escaping.m
@@ -2,6 +2,8 @@
 //  AKNSString+Escaping.m
 //  Telephone
 //
+//  Copyright (c) 2008-2015 Alexei Kuznetsov
+//
 //  Telephone is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
