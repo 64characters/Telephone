d7deb4b527cf86c3b4d01afefd22e2a022693837
Alexey Kuznetsov <mail@eofster.com>
2016-01-29 16:03:48 +0100

    Add UserDefaultsRingtoneSoundNameSaveInteractor
    Issue #161

---
+++ UseCases/UserDefaultsRingtoneSoundNameSaveInteractor.swift
@@ -0,0 +1,32 @@
+//
+//  UserDefaultsRingtoneSoundNameSaveInteractor.swift
+//  Telephone
+//
+//  Copyright (c) 2008-2016 Alexey Kuznetsov
+//
+//  Telephone is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Telephone is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+
+public class UserDefaultsRingtoneSoundNameSaveInteractor {
+    let soundName: String
+    let userDefaults: UserDefaults
+
+    public init(soundName: String, userDefaults: UserDefaults) {
+        self.soundName = soundName
+        self.userDefaults = userDefaults
+    }
+}
+
+extension UserDefaultsRingtoneSoundNameSaveInteractor: Interactor {
+    public func execute() {
+        userDefaults[kRingingSound] = soundName
+    }
+}
