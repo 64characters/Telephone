5cf084b9cbefd69b19fe92a28efdc1fdea325232
Alexey Kuznetsov <mail@eofster.com>
2016-03-14 12:42:02 +0100

    Add RingtoneOutputUpdateInteractor
    Issue #161

---
+++ UseCases/RingtoneOutputUpdateInteractor.swift
@@ -0,0 +1,33 @@
+//
+//  RingtoneOutputUpdateInteractor.swift
+//  Telephone
+//
+//  Copyright (c) 2008-2016 Alexey Kuznetsov
+//
+//  Telephone is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Telephone is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+
+public class RingtoneOutputUpdateInteractor {
+    public let playback: RingtonePlaybackInteractorInput
+
+    public init(playback: RingtonePlaybackInteractorInput) {
+        self.playback = playback
+    }
+}
+
+extension RingtoneOutputUpdateInteractor: ThrowingInteractor {
+    public func execute() throws {
+        if playback.playing {
+            playback.stopPlayingRingtone()
+            try playback.startPlayingRingtone()
+        }
+    }
+}
