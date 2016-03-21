e5a274d731474032d336879028f5e1ce937e23a0
Alexey Kuznetsov <mail@eofster.com>
2016-03-21 12:08:41 +0100

    Shorter names in RepeatingSoundTests
    Issue #176

--- UseCasesTests/RepeatingSoundTests.swift
+++ UseCasesTests/RepeatingSoundTests.swift
@@ -20,39 +20,39 @@ import UseCasesTestDoubles
 import XCTest
 
 class RepeatingSoundTests: XCTestCase {
-    private(set) var soundSpy: SoundSpy!
-    private(set) var timerFactorySpy: TimerFactorySpy!
-    private(set) var timerSpy: TimerSpy!
+    private(set) var sound: SoundSpy!
+    private(set) var factory: TimerFactorySpy!
+    private(set) var timer: TimerSpy!
     private(set) var sut: RepeatingSound!
 
     override func setUp() {
         super.setUp()
-        soundSpy = SoundSpy(eventTarget: NullSoundEventTarget())
-        timerFactorySpy = TimerFactorySpy()
-        timerSpy = TimerSpy()
-        timerFactorySpy.stubWith(timerSpy)
-        sut = RepeatingSound(sound: soundSpy, interval: 1, factory: timerFactorySpy)
+        sound = SoundSpy(eventTarget: NullSoundEventTarget())
+        factory = TimerFactorySpy()
+        timer = TimerSpy()
+        factory.stubWith(timer)
+        sut = RepeatingSound(sound: sound, interval: 1, factory: factory)
     }
 
     func testCreatesRepeatingTimerOnStartPlaying() {
         sut.startPlaying()
 
-        XCTAssertTrue(timerFactorySpy.didCallCreateRepeatingTimer)
-        XCTAssertEqual(timerFactorySpy.invokedInterval, 1)
+        XCTAssertTrue(factory.didCallCreateRepeatingTimer)
+        XCTAssertEqual(factory.invokedInterval, 1)
     }
 
     func testInvalidatesRepeatingTimerOnStopPlaying() {
         sut.startPlaying()
         sut.stopPlaying()
 
-        XCTAssertTrue(timerSpy.didCallInvalidate)
+        XCTAssertTrue(timer.didCallInvalidate)
     }
 
     func testCreatesTimerOnceOnTwoConsecutiveCallsToStartPlaying() {
         sut.startPlaying()
         sut.startPlaying()
 
-        XCTAssertEqual(timerFactorySpy.createRepeatingTimerCallCount, 1)
+        XCTAssertEqual(factory.createRepeatingTimerCallCount, 1)
     }
 
     func testInvalidatesTimerOnceOnTwoConsecutiveCallsToStopPlaying() {
@@ -60,19 +60,19 @@ class RepeatingSoundTests: XCTestCase {
         sut.stopPlaying()
         sut.stopPlaying()
 
-        XCTAssertEqual(timerSpy.invalidateCallCount, 1)
+        XCTAssertEqual(timer.invalidateCallCount, 1)
     }
 
     func testPlaysSoundOnStartPlaying() {
         sut.startPlaying()
 
-        XCTAssertTrue(soundSpy.didCallPlay)
+        XCTAssertTrue(sound.didCallPlay)
     }
 
     func testStopsSoundOnStopPlaying() {
         sut.startPlaying()
         sut.stopPlaying()
 
-        XCTAssertTrue(soundSpy.didCallStop)
+        XCTAssertTrue(sound.didCallStop)
     }
 }
