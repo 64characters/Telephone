3c708fff340a3d22b1d04244134ed868780261be
Alexey Kuznetsov <mail@eofster.com>
2016-04-15 12:03:54 +0200

    Extract AKSIPCall notifications
    Issue #195

---
+++ Telephone/AKSIPCallNotifications.h
@@ -0,0 +1,51 @@
+//
+//  AKSIPCallNotifications.h
+//  Telephone
+//
+//  Copyright (c) 2008-2016 Alexey Kuznetsov
+//  Copyright (c) 2016 64 Characters
+//
+//  Telephone is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Telephone is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+
+@import Foundation;
+
+// Calling. After INVITE is sent.
+extern NSString * const AKSIPCallCallingNotification;
+//
+// Incoming. After INVITE is received.
+extern NSString * const AKSIPCallIncomingNotification;
+//
+// Early. After response with To tag.
+// Keys: @"AKSIPEventCode", @"AKSIPEventReason".
+extern NSString * const AKSIPCallEarlyNotification;
+//
+// Connecting. After 2xx is sent/received.
+extern NSString * const AKSIPCallConnectingNotification;
+//
+// Confirmed. After ACK is sent/received.
+extern NSString * const AKSIPCallDidConfirmNotification;
+//
+// Disconnected. Session is terminated.
+extern NSString * const AKSIPCallDidDisconnectNotification;
+//
+// Call media is active.
+extern NSString * const AKSIPCallMediaDidBecomeActiveNotification;
+//
+// Call media is put on hold by local endpoint.
+extern NSString * const AKSIPCallDidLocalHoldNotification;
+//
+// Call media is put on hold by remote endpoint.
+extern NSString * const AKSIPCallDidRemoteHoldNotification;
+//
+// Call transfer status changed.
+// Key: @"AKFinalTransferNotification".
+extern NSString * const AKSIPCallTransferStatusDidChangeNotification;
---
+++ Telephone/AKSIPCallNotifications.m
@@ -0,0 +1,30 @@
+//
+//  AKSIPCallNotifications.m
+//  Telephone
+//
+//  Copyright (c) 2008-2016 Alexey Kuznetsov
+//  Copyright (c) 2016 64 Characters
+//
+//  Telephone is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Telephone is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+
+#import "AKSIPCallNotifications.h"
+
+NSString * const AKSIPCallCallingNotification = @"AKSIPCallCalling";
+NSString * const AKSIPCallIncomingNotification = @"AKSIPCallIncoming";
+NSString * const AKSIPCallEarlyNotification = @"AKSIPCallEarly";
+NSString * const AKSIPCallConnectingNotification = @"AKSIPCallConnecting";
+NSString * const AKSIPCallDidConfirmNotification = @"AKSIPCallDidConfirm";
+NSString * const AKSIPCallDidDisconnectNotification = @"AKSIPCallDidDisconnect";
+NSString * const AKSIPCallMediaDidBecomeActiveNotification = @"AKSIPCallMediaDidBecomeActive";
+NSString * const AKSIPCallDidLocalHoldNotification = @"AKSIPCallDidLocalHold";
+NSString * const AKSIPCallDidRemoteHoldNotification = @"AKSIPCallDidRemoteHold";
+NSString * const AKSIPCallTransferStatusDidChangeNotification = @"AKSIPCallTransferStatusDidChange";
