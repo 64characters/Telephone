44c95646abf1c66e19d96d2afc779126bfc93f51
Alexey Kuznetsov <mail@eofster.com>
2016-06-10 14:21:29 +0200

    Dispatch with delay
    Issue #224

--- Telephone/FakeStoreClient.swift
+++ Telephone/FakeStoreClient.swift
@@ -29,7 +29,11 @@ class FakeStoreClient {
 extension FakeStoreClient: StoreClient {
     func fetchProducts(withIdentifiers identifiers: [String]) {
         attempts += 1
-        dispatch_async(dispatch_get_main_queue(), notifyEventTarget)
+        dispatch_after(
+            dispatch_time(DISPATCH_TIME_NOW, Int64(UInt64(1.0) * NSEC_PER_SEC)),
+            dispatch_get_main_queue(),
+            notifyEventTarget
+        )
     }
 
     private func notifyEventTarget() {
