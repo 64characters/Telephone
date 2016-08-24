bfc5612d5c038d99aff05bd15466d4a75d9a7ef1
Alexey Kuznetsov <mail@eofster.com>
2016-08-24 17:18:04 +0200

    Add UserDefaultsSavedAccounts
    Issue #224

---
+++ Telephone/UserDefaultsSavedAccounts.swift
@@ -0,0 +1,37 @@
+//
+//  UserDefaultsSavedAccounts.swift
+//  Telephone
+//
+//  Copyright (c) 2008-2016 Alexey Kuznetsov
+//  Copyright (c) 2016 64 Characters
+//
+//  Telephone is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Telephone is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+
+import UseCases
+
+final class UserDefaultsSavedAccounts {
+    private let defaults: NSUserDefaults
+
+    init(defaults: NSUserDefaults) {
+        self.defaults = defaults
+    }
+}
+
+extension UserDefaultsSavedAccounts: SavedAccounts {
+    var haveEnabled: Bool {
+        return accounts().map({ SavedAccount(dict: $0) }).filter({ $0.isEnabled }).count > 0
+    }
+
+    private func accounts() -> [[String: AnyObject]] {
+        return defaults.arrayForKey(kAccounts) as? [[String: AnyObject]] ?? []
+    }
+}
