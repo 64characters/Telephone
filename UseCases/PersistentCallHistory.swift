298b2940be608bd2e70b3d3b5a462b4b35796c1f
Alexey Kuznetsov <mail@eofster.com>
2016-11-07 15:29:29 +0100

    Add PersistentCallHistory
    Issue #238

---
+++ UseCases/PersistentCallHistory.swift
@@ -0,0 +1,115 @@
+//
+//  PersistentCallHistory.swift
+//  Telephone
+//
+//  Copyright (c) 2008-2016 Alexey Kuznetsov
+//  Copyright (c) 2016 64 Characters
+//
+//  Telephone is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Telephone is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+
+public final class PersistentCallHistory {
+    fileprivate let origin: CallHistory
+    fileprivate let url: URL
+
+    public init(origin: CallHistory, url: URL) {
+        self.origin = origin
+        self.url = url
+        origin.removeAll()
+        populateOriginRecordsFromURL()
+    }
+
+    private func populateOriginRecordsFromURL() {
+        do {
+            addToOrigin(records: try readRecords(from: url))
+        } catch {
+            print("Could not read call history from file: \(error)")
+        }
+    }
+
+    private func addToOrigin(records: Any) {
+        guard let records = records as? NSArray else { return }
+        records.flatMap({ $0 as? [String: Any] }).forEach({ origin.add(CallHistoryRecord(dictionary: $0)) })
+    }
+
+    private func readRecords(from url: URL) throws -> Any {
+        return try PropertyListSerialization.propertyList(from: try Data(contentsOf: url), options: [], format: nil)
+    }
+}
+
+extension PersistentCallHistory: CallHistory {
+    public var allRecords: [CallHistoryRecord] {
+        return origin.allRecords
+    }
+
+    public func add(_ record: CallHistoryRecord) {
+        origin.add(record)
+        save()
+    }
+
+    public func add(_ records: [CallHistoryRecord]) {
+        origin.add(records)
+    }
+
+    public func remove(at index: Int) {
+        origin.remove(at: index)
+        save()
+    }
+
+    public func removeAll() {
+        origin.removeAll()
+        save()
+    }
+
+    private func save() {
+        do {
+            try write(dictionaries(from: origin.allRecords), to: url)
+        } catch {
+            print("Could not save call history to file: \(error)")
+        }
+    }
+}
+
+private extension CallHistoryRecord {
+    init(dictionary: [String: Any]) {
+        accountID = dictionary[accountIDKey] as? String ?? ""
+        user = dictionary[userKey] as? String ?? ""
+        domain = dictionary[domainKey] as? String ?? ""
+        date = dictionary[dateKey] as? Date ?? Date.distantPast
+        isIncoming = dictionary[incomingKey] as? Bool ?? false
+        isMissed = dictionary[missedKey] as? Bool ?? false
+    }
+}
+
+private func dictionaries(from records: [CallHistoryRecord]) -> [[String: Any]] {
+    return records.map {
+        return [
+            accountIDKey: $0.accountID,
+            userKey: $0.user,
+            domainKey: $0.domain,
+            dateKey: $0.date,
+            incomingKey: $0.isIncoming,
+            missedKey: $0.isMissed
+        ]
+    }
+}
+
+private func write(_ plist: [[String: Any]], to url: URL) throws {
+    try PropertyListSerialization.data(fromPropertyList: plist, format: .binary, options: 0)
+        .write(to: url, options: .atomic)
+}
+
+private let accountIDKey = "accountID"
+private let userKey = "user"
+private let domainKey = "domain"
+private let dateKey = "date"
+private let incomingKey = "incoming"
+private let missedKey = "missed"
---
+++ UseCasesTests/PersistentCallHistoryTests.swift
@@ -0,0 +1,117 @@
+//
+//  PersistentCallHistoryTests.swift
+//  Telephone
+//
+//  Copyright (c) 2008-2016 Alexey Kuznetsov
+//  Copyright (c) 2016 64 Characters
+//
+//  Telephone is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Telephone is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+
+import UseCases
+import XCTest
+
+final class PersistentCallHistoryTests: XCTestCase {
+    private var url: URL!
+
+    override func setUp() {
+        super.setUp()
+        url = URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
+            .appendingPathComponent("\(ProcessInfo.processInfo.globallyUniqueString)-call-history.plist")
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        removeFile(at: url)
+    }
+
+    func testPersistsAfterAdding() {
+        let record1 = makeRecord1()
+        let record2 = makeRecord2()
+
+        var sut = PersistentCallHistory(origin: SimpleCallHistory(), url: url)
+        sut.add(record1)
+        sut.add(record2)
+        sut = PersistentCallHistory(origin: SimpleCallHistory(), url: url)
+
+        XCTAssertEqual(sut.allRecords, [record1, record2])
+    }
+
+    func testPersistsAfterRemovingIndividual() {
+        let record1 = makeRecord1()
+        let record2 = makeRecord2()
+
+        var sut = PersistentCallHistory(origin: SimpleCallHistory(), url: url)
+        sut.add(record1)
+        sut.add(record2)
+        sut.remove(at: 0)
+        sut = PersistentCallHistory(origin: SimpleCallHistory(), url: url)
+
+        XCTAssertEqual(sut.allRecords, [record2])
+    }
+
+    func testPersistsAfterRemovingAll() {
+        let record1 = makeRecord1()
+        let record2 = makeRecord2()
+
+        var sut = PersistentCallHistory(origin: SimpleCallHistory(), url: url)
+        sut.add(record1)
+        sut.add(record2)
+        sut.removeAll()
+        sut = PersistentCallHistory(origin: SimpleCallHistory(), url: url)
+
+        XCTAssertEqual(sut.allRecords, [])
+    }
+
+    func testDiscardsOriginContentAfterCreation() {
+        let origin = SimpleCallHistory()
+        origin.add(makeRecord1())
+        origin.add(makeRecord2())
+
+        let sut = PersistentCallHistory(origin: origin, url: url)
+
+        XCTAssertEqual(sut.allRecords.count, 0)
+    }
+}
+
+private func makeRecord1() -> CallHistoryRecord {
+    return CallHistoryRecord(
+        accountID: "any-accountID",
+        user: "any-user",
+        domain: "any-domain",
+        date: Date(),
+        isIncoming: false,
+        isMissed: false
+    )
+}
+
+private func makeRecord2() -> CallHistoryRecord {
+    return CallHistoryRecord(
+        accountID: "other-accountID",
+        user: "other-user",
+        domain: "other-domain",
+        date: Date.distantPast,
+        isIncoming: true,
+        isMissed: true
+    )
+}
+
+private func removeFile(at url: URL) {
+    do {
+        try FileManager.default.removeItem(at: url)
+    } catch let error as NSError {
+        if !(error.domain == NSCocoaErrorDomain && error.code == NSFileNoSuchFileError) {
+            XCTFail()
+        }
+    } catch {
+        XCTFail()
+    }
+}
