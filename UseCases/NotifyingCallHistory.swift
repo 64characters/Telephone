0f4eefda1550fe359466a26d85cacebae5e8c483
Alexey Kuznetsov <mail@eofster.com>
2016-11-08 13:38:20 +0100

    Add NotifyingCallHistory
    Issue #238

---
+++ UseCases/NotifyingCallHistory.swift
@@ -0,0 +1,48 @@
+//
+//  NotifyingCallHistory.swift
+//  Telephone
+//
+//  Copyright (c) 2008-2016 Alexey Kuznetsov
+//  Copyright (c) 2016 64 Characters
+//
+//  Telephone is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Telephone is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+
+public class NotifyingCallHistory {
+    fileprivate let origin: CallHistory
+    fileprivate let target: CallHistoryEventTarget
+
+    public init(origin: CallHistory, target: CallHistoryEventTarget) {
+        self.origin = origin
+        self.target = target
+    }
+}
+
+extension NotifyingCallHistory: CallHistory {
+    public var allRecords: [CallHistoryRecord] {
+        return origin.allRecords
+    }
+
+    public func add(_ record: CallHistoryRecord) {
+        origin.add(record)
+        target.callHistoryDidUpdate(self)
+    }
+
+    public func remove(_ record: CallHistoryRecord) {
+        origin.remove(record)
+        target.callHistoryDidUpdate(self)
+    }
+
+    public func removeAll() {
+        origin.removeAll()
+        target.callHistoryDidUpdate(self)
+    }
+}
---
+++ UseCasesTests/NotifyingCallHistoryTests.swift
@@ -0,0 +1,56 @@
+//
+//  NotifyingCallHistoryTests.swift
+//  Telephone
+//
+//  Copyright (c) 2008-2016 Alexey Kuznetsov
+//  Copyright (c) 2016 64 Characters
+//
+//  Telephone is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Telephone is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+
+import UseCases
+import UseCasesTestDoubles
+import XCTest
+
+final class NotifyingCallHistoryTests: XCTestCase {
+    func testNotifiesTargetAfterAdding() {
+        let target = CallHistoryEventTargetSpy()
+        let sut = NotifyingCallHistory(origin: TruncatingCallHistory(), target: target)
+        let factory = CallHistoryRecordTestFactory()
+
+        sut.add(factory.makeRecord(number: 1))
+
+        XCTAssertTrue(target.didCallDidUpdate)
+    }
+
+    func testNotifiesTargetAfterRemovingIndividual() {
+        let target = CallHistoryEventTargetSpy()
+        let sut = NotifyingCallHistory(origin: TruncatingCallHistory(), target: target)
+        let record = CallHistoryRecordTestFactory().makeRecord(number: 1)
+
+        sut.add(record)
+        sut.remove(record)
+
+        XCTAssertEqual(target.didUpdateCallCount, 2)
+    }
+
+    func testNotifiesTargetAfterRemovingAll() {
+        let target = CallHistoryEventTargetSpy()
+        let sut = NotifyingCallHistory(origin: TruncatingCallHistory(), target: target)
+        let factory = CallHistoryRecordTestFactory()
+
+        sut.add(factory.makeRecord(number: 1))
+        sut.add(factory.makeRecord(number: 2))
+        sut.removeAll()
+
+        XCTAssertEqual(target.didUpdateCallCount, 3)
+    }
+}
