1c58e361d7d9d52aa1213e0df1867140de215146
Alexey Kuznetsov <mail@eofster.com>
2016-11-08 11:58:41 +0100

    Add TruncatingCallHistory
    Issue #238

---
+++ UseCases/TruncatingCallHistory.swift
@@ -0,0 +1,48 @@
+//
+//  TruncatingCallHistory.swift
+//  Telephone
+//
+//  Copyright (c) 2008-2016 Alexey Kuznetsov
+//  Copyright (c) 2016 64 Characters
+//
+//  Telephone is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Telephone is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+
+public class TruncatingCallHistory {
+    fileprivate var records = [CallHistoryRecord]()
+
+    fileprivate let limit: Int
+
+    public init(limit: Int) {
+        self.limit = limit
+    }
+}
+
+extension TruncatingCallHistory: CallHistory {
+    public var allRecords: [CallHistoryRecord] {
+        return records
+    }
+
+    public func add(_ record: CallHistoryRecord) {
+        records.append(record)
+        if records.count > limit {
+            records = [CallHistoryRecord](records.suffix(limit))
+        }
+    }
+
+    public func remove(at index: Int) {
+        records.remove(at: index)
+    }
+
+    public func removeAll() {
+        records.removeAll()
+    }
+}
