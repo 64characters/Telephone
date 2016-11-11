e05a889ca18e852d1b7dd2de65005e7f21afe31b
Alexey Kuznetsov <mail@eofster.com>
2016-11-11 14:09:33 +0100

    Add CallHistoryRecordRemoveUseCase
    Issue #238

---
+++ UseCasesTests/CallHistoryRecordRemoveUseCaseTests.swift
@@ -0,0 +1,34 @@
+//
+//  CallHistoryRecordRemoveUseCaseTests.swift
+//  Telephone
+//
+//  Copyright (c) 2008-2016 Alexey Kuznetsov
+//  Copyright (c) 2016 64 Characters
+//
+//  Telephone is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Telephone is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+
+import UseCases
+import UseCasesTestDoubles
+import XCTest
+
+final class CallHistoryRecordRemoveUseCaseTests: XCTestCase {
+    func testRemovesRecord() {
+        let record = CallHistoryRecordTestFactory().makeRecord(number: 1)
+        let history = TruncatingCallHistory()
+        history.add(record)
+        let sut = CallHistoryRecordRemoveUseCase(history: history, record: record)
+
+        sut.execute()
+
+        XCTAssertEqual(history.allRecords.count, 0)
+    }
+}
