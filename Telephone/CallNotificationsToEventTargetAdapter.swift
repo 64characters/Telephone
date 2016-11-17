814552ad523b6f5d825f5b3bef4f33d1a9178e2a
Alexey Kuznetsov <mail@eofster.com>
2016-11-17 14:23:24 +0100

    Add CallNotificationsToEventTargetAdapter
    Issue #238

---
+++ Telephone/CallNotificationsToEventTargetAdapter.swift
@@ -0,0 +1,53 @@
+//
+//  CallNotificationsToEventTargetAdapter.swift
+//  Telephone
+//
+//  Copyright (c) 2008-2016 Alexey Kuznetsov
+//  Copyright (c) 2016 64 Characters
+//
+//  Telephone is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Telephone is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+
+import Foundation
+import UseCases
+
+final class CallNotificationsToEventTargetAdapter {
+    private let center: NotificationCenter
+    private let target: CallEventTarget
+
+    init(center: NotificationCenter, target: CallEventTarget) {
+        self.center = center
+        self.target = target
+        center.addObserver(
+            self,
+            selector: #selector(SIPCallDidDisconnect(_:)),
+            name: Notification.Name.AKSIPCallDidDisconnect,
+            object: nil
+        )
+    }
+
+    deinit {
+        center.removeObserver(self, name: Notification.Name.AKSIPCallDidDisconnect, object: nil)
+    }
+
+    @objc private func SIPCallDidDisconnect(_ notification: Notification) {
+        target.callDidDisconnect(
+            SimpleCall(
+                accountID: "",
+                remote: URI(user: "", host: ""),
+                date: Date(),
+                duration: 0,
+                isIncoming: false,
+                isMissed: false
+            )
+        )
+    }
+}
---
+++ TelephoneTests/CallNotificationsToEventTargetAdapterTests.swift
@@ -0,0 +1,34 @@
+//
+//  CallNotificationsToEventTargetAdapterTests.swift
+//  Telephone
+//
+//  Copyright (c) 2008-2016 Alexey Kuznetsov
+//  Copyright (c) 2016 64 Characters
+//
+//  Telephone is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Telephone is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+
+import UseCases
+import UseCasesTestDoubles
+import XCTest
+
+class CallNotificationsToEventTargetAdapterTests: XCTestCase {
+    func testCallsDidDisconnect() {
+        let center = NotificationCenter.default
+        let target = CallEventTargetSpy()
+        withExtendedLifetime(CallNotificationsToEventTargetAdapter(center: center, target: target)) {
+
+            center.post(Notification(name: Notification.Name.AKSIPCallDidDisconnect, object: nil, userInfo: nil))
+
+            XCTAssertTrue(target.didCallCallDidDisconnect)
+        }
+    }
+}
