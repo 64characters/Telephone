83b897e504964b8a768d90c62b5745bf62913916
Alexey Kuznetsov <mail@eofster.com>
2017-03-15 17:49:23 +0100

    Add ReversedCallHistory
    Issue #238

---
+++ UseCases/ReversedCallHistory.swift
@@ -0,0 +1,47 @@
+//
+//  ReversedCallHistory.swift
+//  Telephone
+//
+//  Copyright © 2008-2016 Alexey Kuznetsov
+//  Copyright © 2016-2017 64 Characters
+//
+//  Telephone is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Telephone is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+
+public final class ReversedCallHistory {
+    fileprivate let origin: CallHistory
+
+    public init(origin: CallHistory) {
+        self.origin = origin
+    }
+}
+
+extension ReversedCallHistory: CallHistory {
+    public var allRecords: [CallHistoryRecord] {
+        return origin.allRecords.reversed()
+    }
+
+    public func add(_ record: CallHistoryRecord) {
+        origin.add(record)
+    }
+
+    public func remove(_ record: CallHistoryRecord) {
+        origin.remove(record)
+    }
+
+    public func removeAll() {
+        origin.removeAll()
+    }
+
+    public func updateTarget(_ target: CallHistoryEventTarget) {
+        origin.updateTarget(target)
+    }
+}
---
+++ UseCasesTests/ReversedCallHistoryTests.swift
@@ -0,0 +1,87 @@
+//
+//  ReversedCallHistoryTests.swift
+//  Telephone
+//
+//  Copyright © 2008-2016 Alexey Kuznetsov
+//  Copyright © 2016-2017 64 Characters
+//
+//  Telephone is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Telephone is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+
+import UseCases
+import UseCasesTestDoubles
+import XCTest
+
+final class ReversedCallHistoryTests: XCTestCase {
+    func testReturnsAllRecordsReversed() {
+        let factory = CallHistoryRecordTestFactory()
+        let record1 = factory.makeRecord(number: 1)
+        let record2 = factory.makeRecord(number: 2)
+        let record3 = factory.makeRecord(number: 3)
+        let sut = ReversedCallHistory(origin: TruncatingCallHistory())
+        sut.add(record1)
+        sut.add(record2)
+        sut.add(record3)
+
+        XCTAssertEqual(sut.allRecords, [record3, record2, record1])
+    }
+
+    func testAddsRecordsToOriginOnAdd() {
+        let factory = CallHistoryRecordTestFactory()
+        let record1 = factory.makeRecord(number: 1)
+        let record2 = factory.makeRecord(number: 2)
+        let origin = TruncatingCallHistory()
+        let sut = ReversedCallHistory(origin: origin)
+
+        sut.add(record1)
+        sut.add(record2)
+
+        XCTAssertEqual(origin.allRecords, [record1, record2])
+    }
+
+    func testRemovesRecordsFromOriginOnRemove() {
+        let factory = CallHistoryRecordTestFactory()
+        let record1 = factory.makeRecord(number: 1)
+        let record2 = factory.makeRecord(number: 2)
+        let origin = TruncatingCallHistory()
+        let sut = ReversedCallHistory(origin: origin)
+        sut.add(record1)
+        sut.add(record2)
+
+        sut.remove(record1)
+
+        XCTAssertEqual(origin.allRecords, [record2])
+    }
+
+    func testRemovesAllRecordsFromOriginOnRemoveAll() {
+        let factory = CallHistoryRecordTestFactory()
+        let record1 = factory.makeRecord(number: 1)
+        let record2 = factory.makeRecord(number: 2)
+        let origin = TruncatingCallHistory()
+        let sut = ReversedCallHistory(origin: origin)
+        sut.add(record1)
+        sut.add(record2)
+
+        sut.removeAll()
+
+        XCTAssertTrue(origin.allRecords.isEmpty)
+    }
+
+    func testUpdatesTargetOnOriginOnUpdateTarget() {
+        let sut = ReversedCallHistory(origin: NotifyingCallHistory(origin: TruncatingCallHistory()))
+        let target = CallHistoryEventTargetSpy()
+
+        sut.updateTarget(target)
+        sut.add(CallHistoryRecordTestFactory().makeRecord(number: 1))
+
+        XCTAssertTrue(target.didCallDidUpdate)
+    }
+}
