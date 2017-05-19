77c610da868dafcd3fa48eee4663b6b6743c568e
Alexey Kuznetsov <mail@eofster.com>
2017-05-19 18:28:31 +0200

    Add SimpleContactMatchingSettings
    Issue #238

---
+++ UseCases/SimpleContactMatchingSettings.swift
@@ -0,0 +1,34 @@
+//
+//  SimpleContactMatchingSettings.swift
+//  Telephone
+//
+//  Copyright © 2008-2016 Alexey Kuznetsov
+//  Copyright © 2016-2017 64 Characters
+//
+//  Telephone is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Telephone is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+
+public final class SimpleContactMatchingSettings {
+    fileprivate let settings: KeyValueSettings
+
+    public init(settings: KeyValueSettings) {
+        self.settings = settings
+        settings.register(defaults: [key: 9])
+    }
+}
+
+extension SimpleContactMatchingSettings: ContactMatchingSettings {
+    public var significantPhoneNumberLength: Int {
+        return settings.integer(forKey: key)
+    }
+}
+
+private let key = "SignificantPhoneNumberLength"
