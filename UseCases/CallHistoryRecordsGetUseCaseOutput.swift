64859285702437b390506eeeb758ff9654cd5734
Alexey Kuznetsov <mail@eofster.com>
2017-05-31 13:24:42 +0200

    Move protocol declaration to its own file
    Issue #238

--- UseCases/CallHistoryRecordsGetUseCaseOutput.swift
+++ UseCases/CallHistoryRecordsGetUseCaseOutput.swift
@@ -1,5 +1,5 @@
 //
-//  CallHistoryRecordsGetUseCase.swift
+//  CallHistoryRecordsGetUseCaseOutput.swift
 //  Telephone
 //
 //  Copyright © 2008-2016 Alexey Kuznetsov
@@ -16,18 +16,6 @@
 //  GNU General Public License for more details.
 //
 
-public final class CallHistoryRecordsGetUseCase {
-    fileprivate let history: CallHistory
-    fileprivate let output: CallHistoryRecordsGetUseCaseOutput
-
-    public init(history: CallHistory, output: CallHistoryRecordsGetUseCaseOutput) {
-        self.history = history
-        self.output = output
-    }
-}
-
-extension CallHistoryRecordsGetUseCase: UseCase {
-    public func execute() {
-        output.update(records: history.allRecords)
-    }
+public protocol CallHistoryRecordsGetUseCaseOutput {
+    func update(records: [CallHistoryRecord])
 }
