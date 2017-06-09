4fe602880b5afc12007685e8022f12d768181b6c
Alexey Kuznetsov <mail@eofster.com>
2017-06-09 10:12:46 +0200

    Add WeakCallHistoryEventTarget
    Issue #238

---
+++ UseCases/WeakCallHistoryEventTarget.swift
@@ -0,0 +1,31 @@
+//
+//  WeakCallHistoryEventTarget.swift
+//  Telephone
+//
+//  Copyright © 2008-2016 Alexey Kuznetsov
+//  Copyright © 2016-2017 64 Characters
+//
+//  Telephone is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Telephone is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+
+public final class WeakCallHistoryEventTarget {
+    fileprivate weak var origin: CallHistoryEventTarget?
+
+    public init(origin: CallHistoryEventTarget) {
+        self.origin = origin
+    }
+}
+
+extension WeakCallHistoryEventTarget: CallHistoryEventTarget {
+    public func didUpdate(_ history: CallHistory) {
+        origin?.didUpdate(history)
+    }
+}
