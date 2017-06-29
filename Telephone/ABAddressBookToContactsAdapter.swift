6ebf1969f200565da8dff99995fe757c71229579
Alexey Kuznetsov <mail@eofster.com>
2017-06-29 17:15:52 +0200

    Create ABAddressBook before each enumeration
    According to the documentation, the (not shared) ABAddressBook
    instance is usually used for short-time purposes, and the changes to
    the user's address book are not automatically merged into it. So in
    our case, it should be created before each enumeration and discarded
    to contain the recent changes each time.
    
    Issue #238

--- Telephone/ABAddressBookToContactsAdapter.swift
+++ Telephone/ABAddressBookToContactsAdapter.swift
@@ -19,14 +19,15 @@
 import AddressBook
 import UseCases
 
-final class ABAddressBookToContactsAdapter {
-    fileprivate lazy var book: ABAddressBook? = ABAddressBook()
-    fileprivate lazy var nameOrdering: Int = self.book?.defaultNameOrdering() ?? Int(kABFirstNameFirst)
-}
+final class ABAddressBookToContactsAdapter {}
 
 extension ABAddressBookToContactsAdapter: Contacts {
     func enumerate(_ body: @escaping (Contact) -> Void) {
-        guard let book = self.book else { return }
+        guard let book = ABAddressBook() as ABAddressBook? else { return }
+        enumerateRecords(in: book, nameOrdering: book.defaultNameOrdering(), body: body)
+    }
+
+    private func enumerateRecords(in book: ABAddressBook, nameOrdering: Int, body: @escaping (Contact) -> Void) {
         for record in book.people() {
             if let person = record as? ABPerson {
                 body(Contact.init(person, nameOrdering: nameOrdering))
