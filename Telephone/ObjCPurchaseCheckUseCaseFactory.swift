7efa9df77c483d7d5f6a9e20d4f78754464618a7
Alexey Kuznetsov <mail@eofster.com>
2017-08-04 18:28:07 +0200

    Add ObjCPurchaseCheckUseCaseFactory
    Issue #238

---
+++ Telephone/ObjCPurchaseCheckUseCaseFactory.swift
@@ -0,0 +1,31 @@
+//
+//  ObjCPurchaseCheckUseCaseFactory.swift
+//  Telephone
+//
+//  Copyright © 2008-2016 Alexey Kuznetsov
+//  Copyright © 2016-2017 64 Characters
+//
+//  Telephone is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Telephone is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+
+import UseCases
+
+final class ObjCPurchaseCheckUseCaseFactory: NSObject {
+    private let factory: StoreUseCaseFactory
+
+    init(factory: StoreUseCaseFactory) {
+        self.factory = factory
+    }
+
+    func make(output: ObjCPurchaseCheckUseCaseOutput) -> ObjCPurchaseCheckUseCase {
+        return ObjCPurchaseCheckUseCase(factory: factory, output: output)
+    }
+}
