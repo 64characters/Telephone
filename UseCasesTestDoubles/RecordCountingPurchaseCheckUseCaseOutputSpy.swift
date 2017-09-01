0f620585207fe9399ff541909381d68975b7087b
Alexey Kuznetsov <mail@eofster.com>
2017-09-01 13:36:39 +0200

    Add RecordCountingPurchaseCheckUseCase
    Issue #238

---
+++ UseCasesTestDoubles/RecordCountingPurchaseCheckUseCaseOutputSpy.swift
@@ -0,0 +1,38 @@
+//
+//  RecordCountingPurchaseCheckUseCaseOutputSpy.swift
+//  Telephone
+//
+//  Copyright © 2008-2016 Alexey Kuznetsov
+//  Copyright © 2016-2017 64 Characters
+//
+//  Telephone is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Telephone is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+
+import UseCases
+
+public final class RecordCountingPurchaseCheckUseCaseOutputSpy {
+    public fileprivate(set) var didCallDidCheckPurchase = false
+    public fileprivate(set) var invokedCount: Int?
+    public fileprivate(set) var didCallDidFailCheckingPurchase = false
+
+    public init() {}
+}
+
+extension RecordCountingPurchaseCheckUseCaseOutputSpy: RecordCountingPurchaseCheckUseCaseOutput {
+    public func didCheckPurchase(recordCount count: Int) {
+        didCallDidCheckPurchase = true
+        invokedCount = count
+    }
+
+    public func didFailCheckingPurchase() {
+        didCallDidFailCheckingPurchase = true
+    }
+}
