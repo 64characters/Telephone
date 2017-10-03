29f2a536d0c212f1f3f6d98b6547f3dfa880d5bf
Alexey Kuznetsov <mail@eofster.com>
2017-10-03 19:48:06 +0200

    Add SanitizedCallDestination
    Issue #324

---
+++ Telephone/SanitizedCallDestination.swift
@@ -0,0 +1,45 @@
+//
+//  SanitizedCallDestination.swift
+//  Telephone
+//
+//  Copyright © 2008-2016 Alexey Kuznetsov
+//  Copyright © 2016-2017 64 Characters
+//
+//  Telephone is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Telephone is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+
+import Foundation
+
+final class SanitizedCallDestination: NSObject {
+    let value: String
+
+    init(_ string: String) {
+        value = strippingSlashes(from: strippingHeaders(from: string))
+    }
+}
+
+private func strippingSlashes(from string: String) -> String {
+    if let range = string.range(of: "sip://") {
+        return string.replacingCharacters(in: range, with: "sip:")
+    } else if let range = string.range(of: "tel://") {
+        return string.replacingCharacters(in: range, with: "tel:")
+    } else {
+        return string
+    }
+}
+
+private func strippingHeaders(from string: String) -> String {
+    if let range = string.range(of: "?") {
+        return String(string[..<range.lowerBound])
+    } else {
+        return string
+    }
+}
