03c711f57cbed0e5fc3db67f1a2d1d8745b0c706
Alexey Kuznetsov <mail@eofster.com>
2018-01-04 16:57:50 +0100

    Add LogFileURL
    Issue #384

---
+++ TelephoneTests/ApplicationDataLocationsFake.swift
@@ -0,0 +1,33 @@
+//
+//  ApplicationDataLocationsFake.swift
+//  TelephoneTests
+//
+//  Copyright © 2008-2016 Alexey Kuznetsov
+//  Copyright © 2016-2017 64 Characters
+//
+//  Telephone is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Telephone is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+
+final class ApplicationDataLocationsFake: ApplicationDataLocations {
+    private let base = URL(string: NSTemporaryDirectory())!.appendingPathComponent("locations-fake")
+
+    func root() -> URL {
+        return base
+    }
+
+    func logs() -> URL {
+        return base.appendingPathComponent("logs")
+    }
+
+    func callHistories() -> URL {
+        return base.appendingPathComponent("histories")
+    }
+}
---
+++ TelephoneTests/LogFileURLTests.swift
@@ -0,0 +1,41 @@
+//
+//  LogFileURLTests.swift
+//  TelephoneTests
+//
+//  Copyright © 2008-2016 Alexey Kuznetsov
+//  Copyright © 2016-2017 64 Characters
+//
+//  Telephone is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Telephone is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+
+import XCTest
+
+final class LogFileURLTests: XCTestCase {
+    func testURLValueIsLogsLocationPlusFileName() {
+        let locations = ApplicationDataLocationsFake()
+        let filename = "any"
+
+        XCTAssertEqual(
+            LogFileURL(locations: locations, filename: filename).urlValue,
+            locations.logs().appendingPathComponent(filename)
+        )
+    }
+
+    func testPathValueIsPathValueFromURL() {
+        let locations = ApplicationDataLocationsFake()
+        let filename = "any"
+
+        XCTAssertEqual(
+            LogFileURL(locations: locations, filename: filename).pathValue,
+            LogFileURL(locations: locations, filename: filename).urlValue.path
+        )
+    }
+}
