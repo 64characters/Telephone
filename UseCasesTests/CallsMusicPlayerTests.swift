c47260209a91cc9bc4c83af416bf18e4cbff6b5f
Alexey Kuznetsov <mail@eofster.com>
2018-03-02 18:20:25 +0100

    Add CallsMusicPlayer
    Issue #429

---
+++ UseCasesTests/CallsMusicPlayerTests.swift
@@ -0,0 +1,59 @@
+//
+//  CallsMusicPlayerTests.swift
+//  Telephone
+//
+//  Copyright © 2008-2016 Alexey Kuznetsov
+//  Copyright © 2016-2018 64 Characters
+//
+//  Telephone is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Telephone is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+
+import UseCases
+import UseCasesTestDoubles
+import XCTest
+
+final class CallsMusicPlayerTests: XCTestCase {
+    func testResumesWhenThereAreNoActiveCalls() {
+        let origin = MusicPlayerSpy()
+        let sut = CallsMusicPlayer(origin: origin, calls: NoActiveCallsStub())
+
+        sut.resume()
+
+        XCTAssertTrue(origin.didCallResume)
+    }
+
+    func testDoesNotResumeWhenThereAreActiveCalls() {
+        let origin = MusicPlayerSpy()
+        let sut = CallsMusicPlayer(origin: origin, calls: ActiveCallsStub())
+
+        sut.resume()
+
+        XCTAssertFalse(origin.didCallResume)
+    }
+
+    func testPausesWhenThereAreNoActiveCalls() {
+        let origin = MusicPlayerSpy()
+        let sut = CallsMusicPlayer(origin: origin, calls: NoActiveCallsStub())
+
+        sut.pause()
+
+        XCTAssertTrue(origin.didCallPause)
+    }
+
+    func testPausesWhenThereAreActiveCalls() {
+        let origin = MusicPlayerSpy()
+        let sut = CallsMusicPlayer(origin: origin, calls: ActiveCallsStub())
+
+        sut.pause()
+
+        XCTAssertTrue(origin.didCallPause)
+    }
+}
