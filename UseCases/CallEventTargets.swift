59e55ef2b27857c70cfaa92f3c000d5acf26aff2
Alexey Kuznetsov <mail@eofster.com>
2018-03-03 21:24:34 +0100

    Add CallEventTargets
    Issue #429

---
+++ UseCases/CallEventTargets.swift
@@ -0,0 +1,39 @@
+//
+//  CallEventTargets.swift
+//  Telephone
+//
+//  Copyright © 2008-2016 Alexey Kuznetsov
+//  Copyright © 2016-2018 64 Characters
+//
+//  Telephone is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Telephone is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+
+public final class CallEventTargets {
+    private let targets: [CallEventTarget]
+
+    public init(targets: [CallEventTarget]) {
+        self.targets = targets
+    }
+}
+
+extension CallEventTargets: CallEventTarget {
+    public func didMake(_ call: Call) {
+        targets.forEach { $0.didMake(call) }
+    }
+
+    public func didReceive(_ call: Call) {
+        targets.forEach { $0.didReceive(call) }
+    }
+
+    public func didDisconnect(_ call: Call) {
+        targets.forEach { $0.didDisconnect(call) }
+    }
+}
