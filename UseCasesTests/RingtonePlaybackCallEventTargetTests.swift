bea897608b8c5cd5bb5e9d18fcc01927c0f12283
Alexey Kuznetsov <mail@eofster.com>
2018-10-09 17:41:45 +0200

    Add RingtonePlaybackCallEventTarget

---
+++ UseCasesTests/RingtonePlaybackCallEventTargetTests.swift
@@ -0,0 +1,41 @@
+//
+//  RingtonePlaybackCallEventTargetTests.swift
+//  Telephone
+//
+//  Copyright © 2008-2016 Alexey Kuznetsov
+//  Copyright © 2016-2018 64 Characters
+//
+//  Telephone is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Telephone is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+
+import UseCases
+import UseCasesTestDoubles
+import XCTest
+
+final class RingtonePlaybackCallEventTargetTests: XCTestCase {
+    func testStopsPlaybackOnIsConnecting() {
+        let playback = RingtonePlaybackUseCaseSpy()
+        let sut = RingtonePlaybackCallEventTarget(playback: playback)
+
+        sut.isConnecting(CallTestFactory().make())
+
+        XCTAssertTrue(playback.didCallStop)
+    }
+
+    func testStopsPlaybackOnDidDisconnect() {
+        let playback = RingtonePlaybackUseCaseSpy()
+        let sut = RingtonePlaybackCallEventTarget(playback: playback)
+
+        sut.didDisconnect(CallTestFactory().make())
+
+        XCTAssertTrue(playback.didCallStop)
+    }
+}
