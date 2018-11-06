b52626aa65125a6b0e8ea794bc54214a727847a2
Alexey Kuznetsov <mail@eofster.com>
2018-11-06 17:58:16 +0100

    Add ObjCStoreEventTarget and ObjCStoreEventTargetAdapter

---
+++ TelephoneTests/ObjCStoreEventTargetAdapterTests.swift
@@ -0,0 +1,30 @@
+//
+//  ObjCStoreEventTargetAdapterTests.swift
+//  Telephone
+//
+//  Copyright © 2008-2016 Alexey Kuznetsov
+//  Copyright © 2016-2018 64 Characters
+//
+//  Telephone is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Telephone is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+
+import XCTest
+
+final class ObjCStoreEventTargetAdapterTests: XCTestCase {
+    func testCallsDidPurchaseOnTargetOnDidPurchase() {
+        let target = ObjCStoreEventTargetSpy()
+        let sut = ObjCStoreEventTargetAdapter(target: target)
+
+        sut.didPurchase()
+
+        XCTAssertTrue(target.didCallDidPurchase)
+    }
+}
