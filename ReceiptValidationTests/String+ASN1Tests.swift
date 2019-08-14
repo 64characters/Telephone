8a22d3b718d5e0b732e65da4a05598325c1f3416
Alexey Kuznetsov <mail@eofster.com>
2019-08-14 21:41:22 +0200

    Add String+ASN1Tests

--- ReceiptValidationTests/String+ASN1Tests.swift
+++ ReceiptValidationTests/String+ASN1Tests.swift
@@ -1,5 +1,5 @@
 //
-//  ReceiptValidationTests.swift
+//  String+ASN1Tests.swift
 //  Telephone
 //
 //  Copyright © 2008-2016 Alexey Kuznetsov
@@ -18,12 +18,12 @@
 
 import XCTest
 
-class ReceiptValidationTests: XCTestCase {
-    func testCanPass() {
-
+final class String_ASN1Tests: XCTestCase {
+    func testCanDecodeASN1UTF8String() {
+        XCTAssertEqual(String(ASN1UTF8String: Data([0x0c, 0x07, 0x74, 0x65, 0x73, 0x74, 0x31, 0x32, 0x33])), "test123")
     }
 
-    func testCanFail() {
-        XCTFail()
+    func testCanDecodeASN1IA5String() {
+        XCTAssertEqual(String(ASN1IA5String: Data([0x16, 0x07, 0x74, 0x65, 0x73, 0x74, 0x31, 0x32, 0x33])), "test123")
     }
 }
