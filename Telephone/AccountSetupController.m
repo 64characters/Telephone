2fca2e9fb553bb5dd7e214ac05775c3c96461106
Alexey Kuznetsov <mail@eofster.com>
2020-03-16 18:49:28 +0100

    Update IP address by default for new accounts

--- Telephone/AccountSetupController.m
+++ Telephone/AccountSetupController.m
@@ -92,6 +92,9 @@ NSString * const AKAccountSetupControllerDidAddAccountNotification = @"AKAccount
     accountDict[kUseProxy] = @NO;
     accountDict[kProxyHost] = @"";
     accountDict[kProxyPort] = @0;
+    accountDict[kUpdateContactHeader] = @YES;
+    accountDict[kUpdateViaHeader] = @YES;
+    accountDict[kUpdateSDP] = @YES;
     
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     NSMutableArray *savedAccounts = [NSMutableArray arrayWithArray:[defaults arrayForKey:kAccounts]];
