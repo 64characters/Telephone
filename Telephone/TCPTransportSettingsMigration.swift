1a738037b4323fdffa3d7cd472a0d6acfb3a6779
Alexey Kuznetsov <mail@eofster.com>
2020-04-02 00:57:44 +0200

    Add user defaults migration for TCP transport

---
+++ Telephone/TCPTransportSettingsMigration.swift
@@ -0,0 +1,41 @@
+//
+//  TCPTransportSettingsMigration.swift
+//  Telephone
+//
+//  Copyright © 2008-2016 Alexey Kuznetsov
+//  Copyright © 2016-2020 64 Characters
+//
+//  Telephone is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Telephone is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+
+import UseCases
+
+final class TCPTransportSettingsMigration {
+    private let settings: KeyValueSettings
+
+    init(settings: KeyValueSettings) {
+        self.settings = settings
+    }
+}
+
+extension TCPTransportSettingsMigration: SettingsMigration {
+    func execute() {
+        settings.save(accounts: settings.loadAccounts().map(settingTransportToTCPAndRemovingProxyParameterIfNeeded))
+    }
+}
+
+private func settingTransportToTCPAndRemovingProxyParameterIfNeeded(in dict: [String: Any]) -> [String: Any] {
+    guard let proxy = dict[kProxyHost] as? String, proxy.contains(";transport=tcp") else { return dict }
+    var result = dict
+    result[kTransport] = kTransportTCP
+    result[kProxyHost] = proxy.replacingOccurrences(of: ";transport=tcp", with: "")
+    return result
+}
